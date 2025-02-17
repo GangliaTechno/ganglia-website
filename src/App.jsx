481b747f49977e83edebd65fc6369fafeab0772c
GouthamSrinu <goutham.msd7@gmail.com>
2025-02-17 15:59:33 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+// import { Route, Router } from 'lucide-react'
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AboutUs } from "./Components/AboutUs";
+import Blog from "./Components/Blog";
+import ContactUs from "./Components/ContactUs";
+import Footer from "./Components/Footer";
+import { Home } from "./Components/HomePage/Home";
+import OurTeam from "./Components/HomePage/OurTeam";
+import NavBar from "./Components/NavBar";
+import { OurProducts } from "./Components/OurProducts";
+import OurServices from "./Components/OurServices";
+import ProductDetail from "./Components/ProductDetail";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <div>
+          <NavBar />
+        </div>
+        <div className="  w-full mt-14">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/services" element={<OurServices />} />
+            <Route path="/ourproducts" element={<OurProducts />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/ourteam" element={<OurTeam />} />
+            <Route path="/product-detail/:title" element={<ProductDetail />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
